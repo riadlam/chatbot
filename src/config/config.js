ccf5078d6b557e800e03b1c5fb46c0a756cc0971
riadlam <riadlaamari@gmail.com>
2025-08-12 23:30:01 +0100

    change

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     // Laravel API Configuration
     laravel: {
-        baseUrl: process.env.LARAVEL_API_URL || 'https://api.soexplast.com',
+        baseUrl: process.env.LARAVEL_API_URL || 'http://197.140.142.101:8000',
         timeout: 10000,
     },
 

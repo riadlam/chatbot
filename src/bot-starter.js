169fc5fe7fbf615abaf0bbb30a8d159a06979c0c
riadlam <riadlaamari@gmail.com>
2025-08-13 00:03:53 +0100

    m

--- src/bot-starter.js
+++ src/bot-starter.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 class BotStarter {
     constructor() {
-        this.apiBaseUrl = 'https://chatbot.soexplast.com/api';
+        this.apiBaseUrl = 'http://197.140.142.101:8000/api';
         this.activeBots = new Map();
         this.qrGenerationTimeouts = new Map();
         this.maxQrGenerationTime = 30000; // 30 seconds timeout
